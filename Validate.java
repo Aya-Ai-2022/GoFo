d9f808c8a7149a4515872e1ca1d347de3034a4eb
178ayasabrycs <45335777+178ayasabrycs@users.noreply.github.com>
2020-05-29 07:19:54 +0200

    update validate.java

--- Validate.java
+++ Validate.java
@@ -1,114 +1,152 @@
 
 package GOFO;
 
+/**
+ * validating various types of input.
+ * 
+ * 
+ */
+public final class Validate {
+
+	private static String error;
+
+	private Validate() {
+	}
+
 	/**
-	  validating various types of input.
-	 
-	
+	 * Returns true if a valid username is input.
+	 * 
 	 */
-	public final class Validate
-	{
-		// prevent instantiation
-		private Validate() {}
-		
-		/**
-		 * Returns true if a valid username is input.
-		
-		 */
-		public static boolean username(String input)
-		{
-			if(!input.isEmpty() && !input.contains(" ") && input.matches("[a-zA-Z]+[a-zA-Z0-9]+")){
-				return true;
+	public static User user;
+
+	public static boolean username(String input) {
+		if (!input.isEmpty() && !input.contains(" ") && input.matches("[a-zA-Z]+[a-zA-Z0-9]+")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Returns true if a valid name is input.
+	 * 
+	 */
+	public static boolean name(String input) {
+		if (!input.isEmpty() && input.matches("[a-zA-Z ,]+")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check that email is valid and return true if it is.
+	 * 
+	 */
+	public static boolean email(String input) {
+		// if(!input.isEmpty() && input.contains("@") && input.contains(".")){
+		// return true;
+		// }
+
+		if (!input.isEmpty() && input.matches("^[A-Za-z0-9+_.-]+@+[A-Za-z0-9.]+[.]+[a-z]+$")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check that phone number is valid and return true if it is.
+	 * 
+	 */
+	public static boolean phone(String input) {
+		input = input.replaceAll("[ \\-.()]", "");
+		return input.matches("\\+?(\\d{8}|\\d{10,11})");
+	}
+
+	/**
+	 * Validates account passwords
+	 */
+	public static boolean password(String password) {
+		// check length
+		if (password.length() >= 8 && password.matches("[a-zA-Z0-9]+")) {
+			// loop through each character and check if there is (at least) 1 upper case, 1
+			// lower case and 1 number
+			// the following vars are set to true once an occurrence of each is found. -kg
+			boolean upper = false;
+			boolean lower = false;
+			boolean num = false;
+
+			for (int i = 0; i < password.length(); i++) {
+				// the surrounding if statements prevent extra checks once criteria have been
+				// met. -kg
+				if (!upper)
+					if (Character.isUpperCase(password.charAt(i)))
+						upper = true;
+				if (!lower)
+					if (Character.isLowerCase(password.charAt(i)))
+						lower = true;
+				if (!num)
+					if (Character.isDigit(password.charAt(i)))
+						num = true;
 			}
-			
-			return false;
+
+			// return true if all criteria are met. -kg
+			return upper && lower && num;
 		}
+		return false;
+	}
 
-		
-		/**
-		 * Returns true if a valid name is input.
-	
-		 */
-		public static boolean name(String input)
-		{
-			if(!input.isEmpty() && input.matches("[a-zA-Z ,]+")){
-				return true;
-			}
-			
-			return false;
-		}
+	public static boolean ValidatePlayer(User user) {
+		if (Validate.name(user.getName())) {
+			if (Validate.username(user.getUserName())) {
+				if (Validate.email(user.getEmail())) {
+					if (Validate.phone(user.getPhone())) {
 
-		
-		/**
-		 * Check that email is valid and return true if it is.
+						return true;
+					} else {
+						setError("Please enter a valid phone number");
 
-		 */
-		public static boolean email(String input)
-		{
-			//if(!input.isEmpty() && input.contains("@") && input.contains(".")){
-			//	return true;
-			//}
-			
-			if(!input.isEmpty() && input.matches("^[A-Za-z0-9+_.-]+@+[A-Za-z0-9.]+[.]+[a-z]+$")){
-				return true;
-			}
-			
-			return false;
-		}
-
-		
-		/**
-		 * Check that phone number is valid and return true if it is.
-
-		 */
-		public static boolean phone(String input)
-		{
-			input = input.replaceAll("[ \\-.()]", "");
-			return input.matches("\\+?(\\d{8}|\\d{10,11})");
-		}
-
-		
-		/**
-		 * Validates account passwords 
-		 */
-		public static boolean password(String password)
-		{
-			//check length
-			if (password.length() >= 8 && password.matches("[a-zA-Z0-9]+"))
-			{
-				// loop through each character and check if there is (at least) 1 upper case, 1 lower case and 1 number
-				// the following vars are set to true once an occurrence of each is found. -kg
-				boolean upper = false;
-				boolean lower = false;
-				boolean num = false;
-				
-				for (int i = 0; i < password.length(); i++)
-				{
-					// the surrounding if statements prevent extra checks once criteria have been met. -kg
-					if (!upper) if (Character.isUpperCase(password.charAt(i))) upper = true;
-					if (!lower) if (Character.isLowerCase(password.charAt(i))) lower = true;
-					if (!num) if (Character.isDigit(password.charAt(i))) num = true;
+					}
+				} else {
+					setError("Please enter a valid email");
 				}
-				
-				// return true if all criteria are met. -kg
-				return upper && lower && num;
+			} else {
+				setError("Please enter an userName");
 			}
-			return false;
+		} else {
+			setError("Please enter a  name");
 		}
-	
+		return false;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public static void setError(String errr) {
+		error = errr;
+	}
+
 	public static void main(String[] args) {
-	Validate v =new Validate();
-	boolean t,m,i,o;
-	t=v.email("ddd@.com");
-	m=v.phone("0125");
-	i=v.username("goodjob");
-	o=v.email("fiiiff@fnnf.mik");
-	
-	System.out.println("THis email: "+t);
-	System.out.println("THis phone: "+m);
-	System.out.println("THis username :  "+i);
-	System.out.println("THis email: "+o);
-	
+		
+		User user = new User("01005050140", "aaaaagoogk.nit", "mk12@#SSjj11", "ggg hh", "jjjjj");
+		Validate v1 = new Validate();
+		System.out.println(ValidatePlayer(user));
+		System.out.println(v1.getError());
+
+		// sample test
+
+		boolean t, m, i, o;
+		t = Validate.email("ddd@.com");
+		m = Validate.phone("0125");
+		i = Validate.username("goodjob");
+		o = Validate.email("fiiiff@fnnf.mik");
+
+		System.out.println("THis email: " + t);
+		System.out.println("THis phone: " + m);
+		System.out.println("THis username :  " + i);
+		System.out.println("THis email: " + o);
 
 	}
 
