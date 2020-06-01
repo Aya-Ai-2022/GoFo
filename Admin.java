ec6d1c3485850f9c9584ffb0304efd27e241ed3c
samar-yassin <46316871+samar-yassin@users.noreply.github.com>
2020-06-01 10:44:24 +0200

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,125 +1,67 @@
-/**
- * 
- */
-package GOFO;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * @author ayasa
  *
  */
 public class Admin {
+    private Vector<player> players=new Vector<player>();
+    private Vector<playgroundOwner>playgroundOwner=new Vector<playgroundOwner>();
+	private Vector<playground>playgrounds=new Vector<playground>();
+    private String adminName;
+    private String adminPass;
 
-	//private Arraylist<playgroundowner>playgroundOwner;
-//	private Arraylist<playground>playgrounds;    
-	private String adminName;
-	private String adminPass;
-	private  String[]account = {"adminGofo","@dminGoFO##"};
-	private boolean active = false;
 
-//
-	
-	
-	public String getAdminName() {
-		return adminName;
-	}
-	public void setAdminName(String adminName) {
-		this.adminName = adminName;
-	}
-	public String getAdminPass() {
-		return this.adminPass;
-	}
+    public Admin(){
+        adminName="adminGofo";
+        adminPass="@dminGoFO##";
+    }
 
-	/**
-	 * 
-	 * @param adminPass
-	 */
-	public void SetAdminPass(String adminPass) {
-		this.adminPass = adminPass;
-	}
-    public boolean checkEmailPassword()
+    public String getAdminName() {
+        return adminName;
+    }
+    public String getAdminPass() {
+        return adminPass;
+    }
+
+   /* public boolean checkEmailPassword()
     {
 
-        if((adminName.equals(account[0])) && (adminPass.equals(account[1])))
-            return true;
-        else
-            return false;
-    }
-    public void adminLogin(){
-    	
-    	if(checkEmailPassword()==true)
-    		active =true;
-    	
-    }
-    
+        if ((adminName.equals(account[0])) && (adminPass.equals(account[1]))) return true;
+        else return false;
+    }*/
 
-    public void deactivateAccount()
-    {
-         active = false;
+    public void approveplayground(playground e) {
+     //check info if it's true
+     //approve it
+     //let playground owner add it
     }
 
-	public void approveplayground() {
-		// TODO - implement admin.approveplayground
-		throw new UnsupportedOperationException();
-	}
+    public void suspendplayground(playground e) {
+        e.setStatus(false);
+    }
 
-	public void suspendplayground() {
-		// TODO - implement admin.suspendplayground
-		throw new UnsupportedOperationException();
-	}
+    public void deletplayground() {
+        // TODO - implement admin.deletplayground
+        throw new UnsupportedOperationException();
+    }
 
-	public void deletplayground() {
-		// TODO - implement admin.deletplayground
-		throw new UnsupportedOperationException();
-	}
-
-	public void activateplayground() {
-		// TODO - implement admin.activateplayground
-		throw new UnsupportedOperationException();
-	}
-
-	public String getAdminPass() {
-		return this.adminPass;
-	}
-
-	/**
-	 * 
-	 * @param adminPass
-	 */
-	public void SetAdminPass(String adminPass) {
-		this.adminPass = adminPass;
-	}
-	 public static void main(String[] args) 
-	    {
-          Admin A=new Admin();
-	      Scanner input = new Scanner (System.in);  
-
-	      String name;
-	      String password;
+    public void activateplayground(playground e) {
+        e.setStatus(true);
+    }
 
 
-	      System.out.println("Welcome !");
-	      System.out.println("\nEnter your username and password to login to your account.");    
-
-	      System.out.println("Username: ");
-	      name = input.nextLine();
-	      A.setAdminName(name);
-
-	      System.out.println("Password: ");
-	        password = input.nextLine();
-	        A.SetAdminPass(password);
-
-	    
-
-	        if(A.checkEmailPassword())
-	            System.out.println("You are logged in!");
-	        else
-	            System.out.println("The username and password you entered are incorrect.");
-	    }
-
-	
-
-
+    public void addPlayer(player p) {
+        players.add(p);
+    }
+    public void addPlayground(playground g) {
+        playgrounds.add(g);
+    }
+    public void addPlaygroundOwner(playgroundOwner o) {
+        playgroundOwner.add(o);
+    }
 
 }
